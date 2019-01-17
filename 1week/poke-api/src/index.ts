b15829ba70e3777bef3a7b9cfb54e6c9ec8b35d9
blake kruppa <btruppa513@gmail.com>
2019-01-17 10:03:24 -0500

    added some initial template for express

--- 1week/poke-api/src/index.ts
+++ 1week/poke-api/src/index.ts
@@ -1,18 +1,9 @@
-import { Pokemon } from './models/pokemon';
-import { User } from './models/user';
-import { PokemonMove } from './models/pokemon-move';
+import express from 'express';
 
-let peter = new User(1,'peter', 'password', 'peter');
-let kyle = new User(2, 'kyle', 'password', 'kyle');
+const app = express();
 
-let slash = new PokemonMove(1, 'slash', 'normal', 25, 10);
-let hyperBeam = new PokemonMove(2, 'hyper beam', 'normal', 10, 100);
-let blastBurn = new PokemonMove(3, 'blast burn', 'fire', 10, 30);
-let surf = new PokemonMove(4, 'surf', 'water', 20, 75);
+app.get('/', (req, res) => {
+  res.send('Hello From Express')
+})
 
-let pokemon: Pokemon[] = [
-  new Pokemon(4, 'charmander', 5, ['fire'], [slash, blastBurn], peter),
-  new Pokemon(7, 'squirtle', 5, ['water'], [hyperBeam, surf], kyle)
-];
-
-console.log(pokemon);
+app.listen(3000);
