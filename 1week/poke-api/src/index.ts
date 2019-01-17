6b8354a40e1f125258ae0ea436e02ca1fbfd0124
blake kruppa <btruppa513@gmail.com>
2019-01-17 12:06:03 -0500

    added to the express demo

--- 1week/poke-api/src/index.ts
+++ 1week/poke-api/src/index.ts
@@ -1,9 +1,36 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello From Express')
+// set up body parser to convert json body to js object and attach to req
+app.use(bodyParser.json());
+
+// create logging middleware
+app.use((req, res, next) => {
+  console.log(`request was made with url: ${req.url}
+  and method: ${req.method}`);
+  next(); // will pass the request on to search for the next piece of middleware
 })
 
-app.listen(3000);
+
+app.get('/users', (req, res) => {
+  res.send('here are your users');
+})
+
+app.post('/users', (req, res) => {
+  const user = req.body;
+  console.log(user)
+  res.sendStatus(201);
+})
+
+app.get('/pokemon', (req, res) => {
+  res.send('here are your pokemon');
+})
+
+app.get('/pokemon-moves', (req, res) => {
+  res.send('here are all the available pokemon moves');
+})
+
+app.listen(3000); 
+console.log('application started on port: 3000')
